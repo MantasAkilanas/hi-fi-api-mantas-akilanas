aeea5dd5d5926869d513ed72f4c7f35e2fa44ea2
MantasAkilanas <4294018@rts-365.dk>
2017-10-03 14:23:03 +0200

    register login

--- routes/account.js
+++ routes/account.js
@@ -5,21 +5,6 @@ const connection = mysql.createConnection({
     "database": "hifi"
 });
 module.exports = (server) => {
-    server.get('/accountInfo', (req, res) => {
-        const query = `select produkter.billede, produkter.fk_kategori as id, kategori.navn as kategori
-    from produkter inner join
-    kategori on fk_kategori = kategori.id`;
-        connection.query(query, (err, results) => {
-            if (err) {
-                res.status(500);
-                res.end;
-            }
-            else {
-                res.setHeader('Access-Control-Allow-Origin', "*");
-                res.send(results);
-            }
-        });
-    })
     server.post('/createAccount', (req, res) => {
 
         let values = [];
@@ -36,6 +21,9 @@ module.exports = (server) => {
                 console.log(results);
                 if (results.length > 0) {
                     console.log("occupied");
+                    res.json(200,{
+                        "message": "Brugernavn eller email er optaged"
+                    })
                 }
                 else {
                     console.log("accepted");
@@ -48,44 +36,47 @@ module.exports = (server) => {
                         if (err) {
                             console.log(err);
                             res.json(500, {
-                                "message": "Internal Server Error",
+                                "message": "Brugernavn eller email er optaged",
                                 "error": err
                             })
                         }
                         else {
-                            res.json(201, {
+                            res.json(200, {
 
-                                "message": "Data indsat", "error" :"noget"
+                                "message": "Data indsat"
                             })
                         }
                     })
                 }
             }
-
-            //     // results.forEach(function (element) {
-            //     if (brugernavn != element.brugernavn || element.email != email) {
-            // let values = [];
-            // values.push(req.body.brugernavn);
-            // values.push(req.body.password);
-            // values.push(req.body.email);
-            // console.log(values);
-            // connection.execute('insert into account(brugernavn,password,email) values (?,?,?)', values, (err, rows) => {
-            //     if (err) {
-            //         console.log(err);
-            //         res.json(500, {
-            //             "message": "Internal Server Error",
-            //             "error": err
-            //         })
-            //     }
-            //     else {
-            //         res.json(200, {
-
-            //             "message": "Data indsat"
-            //         })
-            //     }
-            // })
-            //     }
-            // });
         })
     });
+    server.post('/login', (req, res) => {
+        
+                let values = [];
+                values.push(req.body.brugernavn);
+                values.push(req.body.password);
+                console.log(values);
+                connection.query(`select * from account where account.brugernavn = ? and account.password = ?`, values, (err, results) => {
+                    if (err) {
+                        console.log("fejl");
+                        console.log(err);
+                    }
+                    else {
+                        console.log("ok");
+                        console.log(results);
+                        if (results.length > 0) {
+                            console.log("occupied");
+                            res.json(200,{
+                                "message": "you have succesfully loged in"
+                            })
+                        }
+                        else {
+                            res.json(200,{
+                                "message":"wrong username or password"
+                            })
+                        }
+                    }
+                })
+            });
 }
